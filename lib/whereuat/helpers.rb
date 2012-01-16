47c0b9ab8d0f6c7f2be69d552dd42b17468dc0be
Ryan Taylor Long <ryan@rtlong.com>
2012-01-16 02:53:34 -0600

    Update lib/whereuat/helpers.rb

--- lib/whereuat/helpers.rb
+++ lib/whereuat/helpers.rb
@@ -1,7 +1,7 @@
 module Whereuat
   module Helpers
     def whereuat
-      wua = "<script type='text/javascript'>$.getScript('assets/whereuat.js');</script>"
+      wua = "<script type='text/javascript'>$.getScript('/assets/whereuat.js');</script>"
 
       wua.respond_to?(:html_safe) ? wua.html_safe : wua
     end
